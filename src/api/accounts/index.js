4d781eea507b57647a0888aa3808ca4a487fd0e2
Fahri Abdul Hakim <fahriabdhakim@yahoo.com>
2018-02-14 18:02:33 +0700

    creating json data on accounts

--- src/api/accounts/index.js
+++ src/api/accounts/index.js
@@ -3,7 +3,14 @@ const router = express.Router()
 
 router.get("/", function(req, res) {
   res.send({
-    data: "USERS"
+    data: {
+      "id": 0,
+      "name": "M. Fahri",
+      "bio": " ",
+      "email": "Fahriabdhakim@yahoo.com",
+      "password": "rahasiabeut",
+      "reset_token": " "      
+    }
   })
 })
 
