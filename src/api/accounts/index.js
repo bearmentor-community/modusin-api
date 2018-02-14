02ea052e44d6d89be441dfaa20693c6da37817b1
fikri <fikrimsanad93@gmail.com>
2018-02-14 17:48:42 +0700

    add user api

--- src/api/accounts/index.js
+++ src/api/accounts/index.js
@@ -1,10 +1,15 @@
 const express = require("express")
 const router = express.Router()
 
-router.get("/", function(req, res) {
+router.get('/',function(req,res){
   res.send({
-    data: "USERS"
+    message:'user data:',
+    user:{
+      name:'Fikri',
+      email:'fikrimsanad93@gmail.com',
+      password:'1234'
+    }
   })
-})
+});
 
 module.exports = router
--- src/api/posts/index.js
+++ src/api/posts/index.js
@@ -3,7 +3,12 @@ const router = express.Router()
 
 router.get("/", function(req, res) {
   res.send({
-    data: "POSTS"
+    message: "POSTS",
+    posting:{
+      title:'How To Build StartUp',
+      desc:'Building a startup is not simply building an execution plan for a business model that the entrepreneur thinks will work, but rather, a search for the actual business model itself.',
+      author:'Fikri'
+    }
   })
 })
 
