02a73c642deeeec1feaa6d1918f3574da366421e
M Haidar Hanif <git@mhaidarhanif.com>
2018-02-16 16:32:16 +0700

    Remove mongodb_uri in root api

--- src/api/controller.js
+++ src/api/controller.js
@@ -2,8 +2,7 @@ module.exports = {
   get: (req, res) => {
     res.send({
       author: "M Haidar Hanif",
-      message: "Modusin API",
-      mongodb_uri: process.env.MONGODB_URI
+      message: "Modusin API"
     })
   }
 }
